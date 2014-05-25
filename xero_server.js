78ca1d52b65557f2291bb15daa1c9b8325d10686
Alex Webster <alex@alexwebster.com>
2014-05-25 18:50:46 +1200

    Update xero_server.js

--- xero_server.js
+++ xero_server.js
@@ -1,42 +1,34 @@
 xero = {};
 
-urls = {
-  //requestToken: "http://requestb.in/1l86m7i1",
+urls = { 
   requestToken: "https://api.xero.com/oauth/RequestToken",
-  authorize: "https://www.xero.com/oauth/Authorize",
+  authorize: "https://api.xero.com/oauth/Authorize",
   accessToken: "https://api.xero.com/oauth/AccessToken",
-  //authenticate: "https://www.xero.com/oauth/authenticate"
+  authenticate: "https://api.xero.com/oauth/Authorize"
 };
 
+xero.whitelistedFields = ['name'];
 
-xero.whitelistedFields = ['id', 'displayName'];
+Oauth.registerService('xero', 1, urls, function(oauthBinding) {
+  
+  // The ServiceData below is hard-coded.
+  // This is because the Xero API does not return the current user.
+  // It would probably be batter to return the orgasnisation id / name from the organisation end point:
+  // URLhttps://api.xero.com/api.xro/2.0/organisation
 
-Oauth.registerService('xero', 1, urls, function (oauthBinding) {
-  var identity = oauthBinding.get('https://api.xero.com/api.xro/json').data;
-
-  var serviceData = {
-    id: identity.UserID,
-    FIrstName: identity.FirstName,
+  var serviceData = {    
+    id: 'xero',
+    name: 'xero',
     accessToken: oauthBinding.accessToken,
     accessTokenSecret: oauthBinding.accessTokenSecret
   };
 
-  var profile = identity.user; //add all xero profile data
-  profile["first_name"] = identity.FirstName; //add name to see in default login buttons
-
-  // include helpful fields from xero
-  var fields = _.pick(identity, xero.whitelistedFields);
-  _.extend(serviceData, fields);
-
   return {
-    serviceData: serviceData,
-    options: {
-      profile: profile
-    }
+    serviceData: serviceData
   };
 });
 
 
-xero.retrieveCredential = function (credentialToken) {
+xero.retrieveCredential = function(credentialToken) {
   return Oauth.retrieveCredential(credentialToken);
 };

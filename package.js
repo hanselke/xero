29cb47bfd67e9eb27aeeba7e7d5875ab325965bc
andy <andy@Goddard.local>
2016-11-15 13:01:14 -0800

    add version constraints so it is publishable

--- package.js
+++ package.js
@@ -6,16 +6,17 @@ Package.describe({
   version: '0.1.1',
   name: 'andylash:xero',
   git: 'https://github.com/Opstarts/xero.git',
+  documentation: null,
 });
 
 
 Package.on_use(function(api) {
-  api.use('templating', 'client');
-  api.use('oauth1', ['client', 'server']);
-  api.use('oauth', ['client', 'server']);
-  api.use('ecmascript', ['client', 'server']);
-  api.use('random', 'client');
-  api.use('service-configuration', ['client', 'server']);
+  api.use('templating@1.2.14', 'client');
+  api.use('oauth1@1.1.10', ['client', 'server']);
+  api.use('oauth@1.1.11', ['client', 'server']);
+  api.use('ecmascript@0.5.8', ['client', 'server']);
+  api.use('random@1.0.10', 'client');
+  api.use('service-configuration@1.0.10', ['client', 'server']);
 
   api.add_files(['xero_configure.html', 'xero_configure.js'], 'client');
 
